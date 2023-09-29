2bdf5c906a0c59c24b54213c43a0184dcdd03b53
roodhouse <rughjm@gmail.com>
2023-09-29 14:10:16 -0500

    adjustments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useMain } from './context/mainContext';
 import Home from './components/Home';
+import CategorySection from './components/shared/CategorySection';
 import Header from './components/shared/Header'
 import Footer from './components/shared/Footer'
 
@@ -24,26 +25,26 @@ function App() {
   return (
     <div id='app' className="App font-display">
       <div id='mainWrapper'>
-        <div id='mainContainer' className='flex justify-center bg-[#131313] xl:block'>
-        <div id='headerWrapper' className='absolute w-full z-30'>
-            <Header />
+        <div id='mainContainer' className='bg-[#131313]'>
+          <div id='headerWrapper' className='absolute w-full z-30'>
+              <Header />
+            </div>
+            <div id='homeWrapper'>
+              <Home />
+            </div>
+            <div id='categoryWrapper' className='bg-white pt-10'>
+              <CategorySection />
+            </div>
+            {/* <div id='productWrapper'>
+              <Product />
+            </div> */}
+            {/* <div id='checkoutWrapper'>
+              <Checkout />
+            </div> */}
+            <div id='footerWrapper'>
+              <Footer />
+            </div>
           </div>
-          <div id='homeWrapper'>
-            <Home />
-          </div>
-          {/* <div id='categoryWrapper'>
-            <Category />
-          </div> */}
-          {/* <div id='productWrapper'>
-            <Product />
-          </div> */}
-          {/* <div id='checkoutWrapper'>
-            <Checkout />
-          </div> */}
-          <div id='footerWrapper'>
-            <Footer />
-          </div>
-        </div>
       </div>
     </div>
   );
