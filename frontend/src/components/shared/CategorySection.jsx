7ba326d4f4bf4a1ea5fa2b0f271988bba974e922
roodhouse <rughjm@gmail.com>
2023-09-29 17:45:51 -0500

    state changes of show text

--- frontend/src/components/shared/CategorySection.jsx
+++ frontend/src/components/shared/CategorySection.jsx
@@ -10,20 +10,20 @@ function CategorySection() {
         
         { categories ? (
           categories.map((category) => (
-            <div key={category.category_id} id={`${category.category_name}SectionContainer`} className='bg-offWhite rounded-lg mb-[68px] flex flex-col items-center h-[165px]'>
-              <div id={`${category.category_name}SectionImage`} className='w-[80px] h-[104px] mb-9 mt-[-52px] bg-cover bg-no-repeat bg-center' style={{backgroundImage: `url(${category.category_thumbnail})`}} />
+            <div key={category.category_id} id={`${category.category_name}SectionContainer`} className='bg-offWhite rounded-lg mb-[68px] flex flex-col items-center h-[165px] cursor-pointer group'>
+              <div id={`${category.category_name}SectionImage`} className='w-[80px] h-[104px] mt-[-40px] bg-cover bg-no-repeat bg-center' style={{backgroundImage: `url(${category.category_thumbnail})`}} />
               <div id={`${category.category_name}SectionTitle`} className='mb-[17px] text-black text-center text-[15px] font-bold leading-normal tracking-[1.071px] uppercase'>
                 <p>{category.category_name}</p>
               </div>
-              <div id={`${category.category_name}ShopContainer`} className='flex items-center text-center justify-center'>
-                <div id={`${category.category_name}Shop`} className='mr-[13px] text-[13px] font-bold leading-normal tracking-[1px] text-black uppercase opacity-50'>
-                  <p>Shop</p>
+              <div id={`${category.category_name}ShopContainer`} className='flex items-center text-center justify-center mb-[22px]'>
+                <div id={`${category.category_name}Shop`} className='mr-[13px] text-[13px] font-bold leading-normal tracking-[1px] text-black uppercase opacity-50 group-hover:opacity-[100]'>
+                  <p className='group-hover:text-deepOrange'>Shop</p>
                 </div>
                 <div id={`${category.category_name}ShopArrow`}>
                   <div id={`${category.category_name}Arrow`} className='w-2 h-[10px] bg-[url("./assets/shared/desktop/icon-arrow-right.svg")]' />
                 </div>
               </div>
-          </div>
+            </div>
           ))
         ) : '' }
       </div>
