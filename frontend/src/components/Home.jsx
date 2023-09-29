ac9ecb3b721c1d6ad61b478d8bbc261c5109d434
roodhouse <rughjm@gmail.com>
2023-09-29 13:36:34 -0500

    home page hero section is done

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <>
         <div id="homeContainer" className='flex justify-center max-w-[375px] md:max-w-[768px] xl:max-w-[100%]'>
-            <div id="heroWrapper" className='relative w-full h-screen overflow-hidden md:h-[729px]'>
+            <div id="heroWrapper" className='relative w-full h-[600px] overflow-hidden md:h-[729px]'>
               <Hero />
             </div>
         </div>
