12666c3e27a74a1256535b137071eaad87937c09
roodhouse <rughjm@gmail.com>
2023-10-01 14:04:45 -0500

    circle work

--- frontend/src/components/home/Promotion.jsx
+++ frontend/src/components/home/Promotion.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Zx9Promotion from './promotion/Zx9Promotion'
 
 function Promotion() {
   return (
-    <div>Promotion</div>
+    <>
+      <div id="promotionSectionContainer" className='mx-6'>
+        <div id="zx9PromotionWrapper">
+          <Zx9Promotion />
+        </div>
+      </div>
+    </>
   )
 }
 
---
+++ frontend/src/components/home/promotion/Zx9Promotion.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Circles from '../../../assets/home/desktop/pattern-circles.svg'
+
+// circles giving me issues, may need to rethink the design to accomodate... save another svg with all bg elements..
+
+function Zx9Promotion() {
+  return (
+    <>
+        <div id="zx9PromotionContainer" className='bg-deepOrange rounded-lg w-327px'>
+            <div id="zx9PromotionImageContainer">
+                <div id="circleImage" className='w-[558px] h-[558px] bg-[url("assets/home/desktop/pattern-circles.svg")]'/>
+                <div id="zx9PromotionImage">
+
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Zx9Promotion
