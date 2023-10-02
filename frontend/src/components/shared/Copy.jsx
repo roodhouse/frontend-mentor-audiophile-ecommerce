3228c71d168ed0d26640ec17596a8a7be0e7d39a
roodhouse <rughjm@gmail.com>
2023-10-02 14:49:31 -0500

    copy tablet done

--- frontend/src/components/shared/Copy.jsx
+++ frontend/src/components/shared/Copy.jsx
@@ -6,14 +6,14 @@ import CopyText from './copy/CopyText'
 function Copy() {
   return (
     <>
-      <div id="copySectionContainer" className='px-6'>
-        <div id="copyImageWrapper" className='mb-10'>
+      <div id="copySectionContainer" className='px-6 md:px-10 flex flex-col items-center'>
+        <div id="copyImageWrapper" className='mb-10 md:mb-[63px]'>
           <CopyImage />
         </div>
-        <div id="copyHeadingWrapper" className='mb-8'>
+        <div id="copyHeadingWrapper" className='mb-8 w-[327px] md:w-[573px]'>
           <CopyHeading />
         </div>
-        <div id="copyTextWrapper">
+        <div id="copyTextWrapper" className='w-[327px] md:w-[573px]'>
           <CopyText />
         </div>
       </div>
--- frontend/src/components/shared/copy/CopyImage.jsx
+++ frontend/src/components/shared/copy/CopyImage.jsx
@@ -4,7 +4,7 @@ function CopyImage() {
   return (
     <>
         <div id="copyImageContainer">
-            <div id="copyImage" className='rounded-lg w-[327px] h-[300px] bg-[url("assets/shared/mobile/image-best-gear.svg")]' />
+            <div id="copyImage" className='rounded-lg w-[327px] md:w-[689px] h-[300px] bg-[url("assets/shared/mobile/image-best-gear.svg")] md:bg-[url("assets/shared/tablet/image-best-gear.svg")]' />
         </div>
     </>
   )
