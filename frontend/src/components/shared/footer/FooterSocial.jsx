fba9e155c51e64a6a722d43e734de44b47a9f040
roodhouse <rughjm@gmail.com>
2023-10-02 17:39:59 -0500

    footer done

--- frontend/src/components/shared/footer/FooterSocial.jsx
+++ frontend/src/components/shared/footer/FooterSocial.jsx
@@ -4,9 +4,9 @@ function FooterSocial() {
   return (
     <>
         <div id="footerSocialContainer" className='flex justify-between w-[104px]'>
-            <div id="facebook" className='w-6 h-6 bg-[url("assets/shared/desktop/icon-facebook.svg")] bg-no-repeat'/>
-            <div id="twitter" className='w-6 h-6 bg-[url("assets/shared/desktop/icon-twitter.svg")] bg-no-repeat'/>
-            <div id="instagram" className='w-6 h-6 bg-[url("assets/shared/desktop/icon-instagram.svg")] bg-no-repeat'/>
+            <div id="facebook" className='w-6 h-6 bg-[url("assets/shared/desktop/icon-facebook.svg")] bg-no-repeat cursor-pointer'/>
+            <div id="twitter" className='w-6 h-6 bg-[url("assets/shared/desktop/icon-twitter.svg")] bg-no-repeat cursor-pointer mt-[2px]'/>
+            <div id="instagram" className='w-6 h-6 bg-[url("assets/shared/desktop/icon-instagram.svg")] bg-no-repeat cursor-pointer'/>
         </div>
     </>
   )
--- frontend/src/components/shared/footer/FooterText.jsx
+++ frontend/src/components/shared/footer/FooterText.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function FooterText() {
   return (
     <>
-        <div id="footerTextContainer" className='text-white text-center text-[15px] leading-[25px] font-medium opacity-50'>
+        <div id="footerTextContainer" className='text-white text-center md:text-left text-[15px] leading-[25px] font-medium opacity-50'>
             <p>Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.</p>
         </div>
     </>
